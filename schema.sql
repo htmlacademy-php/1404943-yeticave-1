2f60d5cdcb93064bc359b085a8df1098db1ca0cc
heroldserg89 <heroldserg89@gmail.com>
2025-12-21 12:02:41 +0300

    добавляет схему бд

---
+++ schema.sql
@@ -0,0 +1,57 @@
+CREATE DATABASE IF NOT EXISTS yeticave
+  DEFAULT CHARACTER SET utf8mb4
+  DEFAULT COLLATE utf8mb4_general_ci;
+USE yeticave;
+
+CREATE TABLE categories
+(
+  id          INT AUTO_INCREMENT PRIMARY KEY,
+  title       VARCHAR(80) NOT NULL,
+  symbol_code VARCHAR(50),
+  UNIQUE INDEX idx_symbol_cole (symbol_code),
+  UNIQUE INDEX idx_title (title)
+);
+
+CREATE TABLE users
+(
+  id         INT AUTO_INCREMENT PRIMARY KEY,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  email      VARCHAR(80) NOT NULL,
+  name       VARCHAR(80) NOT NULL,
+  password   CHAR(60)    NOT NULL,
+  contacts   TEXT        NOT NULL,
+  UNIQUE INDEX idx_email (email)
+);
+
+CREATE TABLE lots
+(
+  id          INT AUTO_INCREMENT PRIMARY KEY,
+  created_at  DATETIME DEFAULT CURRENT_TIMESTAMP,
+  title       VARCHAR(80)  NOT NULL,
+  description TEXT         NOT NULL,
+  img_url     VARCHAR(255) NOT NULL,
+  price_start INT          NOT NULL,
+  end_at      DATE         NOT NULL,
+  price_step  INT          NOT NULL,
+  author_id   INT          NOT NULL,
+  winner_id   INT          NOT NULL,
+  category_id INT          NOT NULL,
+  FOREIGN KEY (author_id) REFERENCES users (id),
+  FOREIGN KEY (winner_id) REFERENCES users (id),
+  FOREIGN KEY (category_id) REFERENCES categories (id),
+  INDEX idx_title (title),
+  INDEX idx_created_ad (created_at)
+);
+
+CREATE TABLE bets
+(
+  id         INT AUTO_INCREMENT PRIMARY KEY,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  price      INT NOT NULL,
+  user_id    INT NOT NULL,
+  lot_id     INT NOT NULL,
+  FOREIGN KEY (user_id) REFERENCES users (id),
+  FOREIGN KEY (lot_id) REFERENCES lots (id),
+  INDEX idx_created_at (created_at)
+);
+
